254fa026ab15369b4cb3265feb245abfb2f4c5bb
Erwa <shangyx@yungoal.com>
2023-03-29 09:32:40 +0800

    init

--- src/components/custom/GithubSite.vue
+++ src/components/custom/GithubSite.vue
@@ -1,8 +1,8 @@
 <template>
   <div class="text-neutral-400">
-    <span>Star on</span>
-    <a href="https://github.com/Chanzhaoyu/chatgpt-bot" target="_blank" class="text-blue-500">
-      GitHub
+    <span>Contact me</span>
+    <a href="https://www.yuque.com/wobushitiegan" target="_blank" class="text-blue-500">
+      yuque
     </a>
   </div>
 </template>
